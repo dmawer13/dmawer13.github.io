a27fcbe00684bf914ef2e4d4ca97fefc6e1b837a
dmawer <dmawer@buffalo.edu>
2021-07-06 14:26:31 -0400

    Initial commit

---
+++ empty-example/sketch.js
@@ -0,0 +1,95 @@
+let cards;
+const key = 'd67987e1-34d3-42a3-8461-f9519de91413';
+//const url = 'https://api.pokemontcg.io/v2/cards?q=id:base1';
+
+let sel;
+let button;
+let cardSet = 'base1';
+
+//let url = 'https://api.pokemontcg.io/v2/cards?q=set.name:' + cardSet;
+
+function setup() {
+    createCanvas(600, 825);
+    textAlign(CENTER);
+    background(0);
+    background(200);
+    sel = createSelect();
+    sel.position(5, 5);
+    // 6/7 update: the options below are all by the ID's according the API.   
+    sel.option('Base', 'base1'); //Base
+    sel.option('Jungle', 'base2'); //Jungle
+    sel.option('Wizards Black Star Promos', 'basep'); //Wizards Black Star Promos
+    sel.option('Fossil', 'base3'); //Fossil
+    sel.option('Base Set 2', 'base4'); //Base Set 2
+    sel.option('Team Rocket', 'base5'); //Team Rocket
+    sel.option('Gym Heroes', 'gym1'); //Gym Heroes
+    sel.option('Gym Challenge', 'gym2'); //Gym Challenge
+    sel.option('Neo Genesis', 'neo1'); //Neo Genesis
+    sel.option('Neo Discovery', 'neo2'); //Neo Discovery
+    sel.option('Southern Islands', 'si1'); //Southern Islands
+    sel.option('Neo Revelation', 'neo3'); //Neo Revelation
+    sel.option('Neo Destiny', 'neo4'); //Neo Destiny
+
+    //  sel.selected('Jungle');
+    sel.changed(setCard);
+
+    button = createButton('Run');
+    button.position(200, 5);
+    button.mousePressed(pingURL);
+    setCard();
+}
+
+function setCard() {
+    cardSet = sel.value();
+    console.log("Just changed the dropdown to: " + cardSet);
+    loadJSON('https://api.pokemontcg.io/v2/cards?q=set.id:' + cardSet, gotData);
+    //    it's the set.name in the query above that's important.  I think I should change to set.id because all id's are single words, no spaces. But then that would make the dropdown ugly because they're a little nonsensical.  Maybe I should have an intermediate step where the dropdown names are readable but then the query goes by the ID "behind the scenes".
+}
+
+function pingURL() {
+    setInterval(firstCardPrint, 20000);
+}
+
+function gotData(data) {
+    cards = data;
+    console.log("gotData ran! Set: " + cardSet);
+}
+
+function setSet() {
+    cardSet = sel.value();
+    loadJSON(url, gotData);
+    firstCardPrint();
+    console.log(cardSet);
+}
+
+function firstCardPrint() {
+    if (cards) {
+        //YOU NEED TO WAIT FOR CARDS TO LOAD BEFORE GOING INTO THE JSON
+        let generateRando = Math.floor(random(0,cards.data.length));
+        
+        let response = cards.data[generateRando].images.large;
+        //        text(response, width/2, height/2 + 20);
+        console.log("This set has " + cards.data.length + " cards");
+        console.log("And I'm printing card number " + generateRando);
+        loadImage(response, img => {
+            image(img, 0, 0);
+        });
+        //defo get rid of this below once ready. Just using it to print JSON response once.
+        //        noLoop();
+    } else {
+        console.log("no cards")
+    }
+}
+
+function timedCardPrint() {
+    //borrowed from firstCardPrint function, but omitted console logs.
+    if (cards) {
+        let generateRando = Math.floor(random(0,cards.data.length));
+        let response = cards.data[generateRando].images.large;
+        loadImage(response, img => {
+            image(img, 0, 0);
+        });
+    }
+}
+
+function draw() {}
